2788a89faba5f487832afa7646e279d11f8b0036
Al Rivera <thealrivera@gmail.com>
2016-05-30 00:29:21 -0400

    leaflet map working

--- imports/client/components/Places.js
+++ imports/client/components/Places.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// Google Photo URL
+//https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=YOUR_API_KEY
+
+
 const Places = ({loc, places}) => (
 	<ul className="table-view">
 		{places.map( function( place ){
